6792f3867c2c6876e2455a33fd2699e36bea585b
LautaroAp <lautaro.ap@gmail.com>
2019-08-16 12:24:35 -0300

    conexion angular + spring

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Client} from '@stomp/stompjs';
+import * as SockJS from 'sockjs-client';
 
 @Component({
   selector: 'app-chat',
@@ -7,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChatComponent implements OnInit {
 
+  private client: Client;
+
   constructor() { }
 
   ngOnInit() {
+    this.client = new Client();
+    /* asignar SockJK al stomp */
+    this.client.webSocketFactory = () => {
+      return new SockJS("http://localhost:8080/chat-websocket"); // endpoint para conectar
+    }
+    /* escuchar cuando con conectamos/desconectamos
+    * el objeto "frame" contiene toda la informacionde nuestra conecxion con el broker */
+    this.client.onConnect = (frame) => {
+      console.log('Conectados: ' + this.client.connected + ' : ' + frame);
+    }
+    /* conexion */
+    this.client.activate();
   }
 
 }
